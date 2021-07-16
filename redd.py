3e499815af3a2ae872a1832c6aa202543a38f625
Tyler Bryk <22185354+tylerbryk@users.noreply.github.com>
2021-07-16 14:35:00 -0400

    New recruitment code

--- redd.py
+++ redd.py
@@ -3,9 +3,16 @@ nr_subred_ch_id = 675444353237516314
 ph_subred_ch_id = 819709189181800481
 reminder_ch_id = 819709189181800480
 # ====================================
-
 import datetime
-import pandas as pd
+
+dfr = {'Danger Zone': 610688204395380757,        # Michael
+       'Stormcloaks': 323148470376726551,        # Ice
+       'Downfall Legend': 7748683076225204326,   # SparkPlug
+       'Game Changers': 323148470376726551,      # Ice
+       'Beasthole': 395777730492235777,          # Peter
+       'Phantom Family': 610688204395380757,     # Michael
+       'Night Raiderz': 384491690410639370       # John
+      }
 
 async def check_subred(ctx, client):
 	if ctx.channel.id != nr_subred_ch_id or not ctx.embeds:
@@ -17,9 +24,10 @@ async def check_subred(ctx, client):
 
 
 async def reminder(client):
-	df = pd.read_csv('reddit_reminders.csv')
-	day = df.loc[df['day'] == datetime.datetime.today().weekday()]
+	df = list(dfr.items())
+	day = datetime.datetime.today().weekday()
+	data = df[day]  # Tuple(clan,id)
 	channel = client.get_channel(reminder_ch_id)
-	rc = client.get_user(int(day['rc_id']))
-	await channel.send('\n**Recruitment Post Reminder**\nPlease post for {} {}'.format(day['clan'].item(), rc.mention))
+	rc = client.get_user(data[1])
+	await channel.send('\n**Recruitment Post Reminder**\nPlease post for {} {}'.format(data[0], rc.mention))
 	return
